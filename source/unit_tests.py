5eba876110a4ef698696e67dde5096ce4db1adff
jonathanvdc <jonathan.vdc@outlook.com>
2018-04-05 18:03:57 +0200

    Test bank keyring functionality

--- source/unit_tests.py
+++ source/unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from Crypto.PublicKey import ECC
 
 from account_holder_device import AccountHolderDevice
 from promissory_note import Check, PromissoryNote, PromissoryNoteDraft
@@ -8,6 +9,17 @@ class TestAccountHolderDevice(unittest.TestCase):
     def test_create(self):
         """Tests that an account holder device can be created."""
         AccountHolderDevice()
+
+    def test_bank_keyring(self):
+        """Tests that the bank keyring functionality of an account
+           holder device works as advertised."""
+        device = AccountHolderDevice()
+        bank_id = 42
+        bank_key = ECC.generate(curve='P-256')
+        assert not device.is_known_bank(bank_id)
+        device.register_bank(bank_id, bank_key)
+        assert device.is_known_bank(bank_id)
+        assert device.get_bank_public_key(bank_id) == bank_key
 
 
 if __name__ == '__main__':
